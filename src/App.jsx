ddbb60799483944ad024f9c685a41bf3854618ab
Jerry Xia <jerrylxia@gmail.com>
2024-01-25 18:49:50 -0500

    Add control functionality to mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,9 @@ import { Canvas } from "@react-three/fiber";
 import Game from "./Game";
 import { useThree, useFrame } from "@react-three/fiber";
 import Overlay from "./Overlay";
-import React from "react";
+import React, { useState } from "react";
 import { PointerLockControls as PointerLockControlsImpl } from "./PointerLockControls";
+import { GameContext } from "./GameContext";
 
 function PointerLockControls() {
   const { camera, gl } = useThree();
@@ -24,28 +25,44 @@ function PointerLockControls() {
 export default function App() {
   const isMobile = window.innerWidth <= 800;
 
+  const [controlsMobile, setControlsMobile] = useState({
+    upPressed: false,
+    downPressed: false,
+    leftPressed: false,
+    rightPressed: false,
+  });
+
+  const handleControlChange = (control, value) => {
+    setControlsMobile((prevControls) => ({
+      ...prevControls,
+      [control]: value,
+    }));
+  };
+
   return (
     <>
-      <Canvas shadows>
-        <directionalLight
-          intensity={1}
-          castShadow={true}
-          shadow-bias={-0.00015}
-          shadow-radius={4}
-          shadow-blur={10}
-          shadow-mapSize={[2048, 2048]}
-          position={[85.0, 80.0, 70.0]}
-          shadow-camera-left={-30}
-          shadow-camera-right={30}
-          shadow-camera-top={30}
-          shadow-camera-bottom={-30}
-        />
-        {/* <Environment files="/img/rustig_koppie_puresky_1k.hdr" background /> */}
-        <Game />
-        {isMobile ? <PointerLockControls /> : <PointerLockControlsr />}
-        <Stats />
-      </Canvas>
-      <Overlay />
+      <GameContext.Provider value={{ controlsMobile, handleControlChange }}>
+        <Canvas shadows>
+          <directionalLight
+            intensity={1}
+            castShadow={true}
+            shadow-bias={-0.00015}
+            shadow-radius={4}
+            shadow-blur={10}
+            shadow-mapSize={[2048, 2048]}
+            position={[85.0, 80.0, 70.0]}
+            shadow-camera-left={-30}
+            shadow-camera-right={30}
+            shadow-camera-top={30}
+            shadow-camera-bottom={-30}
+          />
+          {/* <Environment files="/img/rustig_koppie_puresky_1k.hdr" background /> */}
+          <Game />
+          {isMobile ? <PointerLockControls /> : <PointerLockControlsr />}
+          <Stats />
+        </Canvas>
+        <Overlay />
+      </GameContext.Provider>
     </>
   );
 }
--- src/Overlay.jsx
+++ src/Overlay.jsx
@@ -1,10 +1,41 @@
+import { GameContext } from "./GameContext";
+import { useContext } from "react";
+
 export default function Overlay() {
+  const { handleControlChange } = useContext(GameContext);
+  const isMobile = window.innerWidth <= 800;
+
   return (
-    <div id="instructions">
-      W,A,S,D to move.
-      <br />
-      Space to jump.
-      <br />
-    </div>
+    isMobile && (
+      <div
+        id="controls"
+        style={{ position: "absolute", bottom: "10px", left: "10px" }}
+      >
+        <button
+          onTouchStart={() => handleControlChange("upPressed", true)}
+          onTouchEnd={() => handleControlChange("upPressed", false)}
+        >
+          Up
+        </button>
+        <button
+          onTouchStart={() => handleControlChange("downPressed", true)}
+          onTouchEnd={() => handleControlChange("downPressed", false)}
+        >
+          Down
+        </button>
+        <button
+          onTouchStart={() => handleControlChange("leftPressed", true)}
+          onTouchEnd={() => handleControlChange("leftPressed", false)}
+        >
+          Left
+        </button>
+        <button
+          onTouchStart={() => handleControlChange("rightPressed", true)}
+          onTouchEnd={() => handleControlChange("rightPressed", false)}
+        >
+          Right
+        </button>
+      </div>
+    )
   );
 }
