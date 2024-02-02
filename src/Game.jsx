d35747d9a0cb8442f15f4b198cc63d28d3c70e69
Jerry Xia <jerrylxia@gmail.com>
2024-02-02 01:08:37 -0500

    Move planets to own file

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,6 @@
 import { useGLTF, Environment } from "@react-three/drei";
-import { useLoader, useFrame } from "@react-three/fiber";
 import useOctree from "./useOctree";
 import Player from "./Player";
-import { useRef } from "react";
 import useOctreeHelper from "./useOctreeHelper";
 import Model from "./world-components/Platform";
 import Ocean from "./world-components/Ocean";
@@ -10,7 +8,7 @@ import Clouds from "./world-components/Clouds";
 import GroundFoliage from "./world-components/GroundFoliage";
 import TreeLeaves from "./world-components/TreeLeaves";
 import Icoplant from "./world-components/Icoplant";
-import * as THREE from "three";
+import Planets from "./world-components/Planets";
 
 const hexToVec3 = (hex) => {
   hex = hex.replace(/^#/, "");
@@ -24,20 +22,6 @@ export default function Game() {
   const { nodes, scene } = useGLTF("/octree.glb");
   const octree = useOctree(scene);
   useOctreeHelper(octree);
-  const planet = useLoader(THREE.TextureLoader, "/img/planet.png");
-  const planet2 = useLoader(THREE.TextureLoader, "/img/planet2.png");
-  const planet3 = useLoader(THREE.TextureLoader, "/img/planet3.png");
-  const simpleShadow = useLoader(THREE.TextureLoader, "/img/simpleShadow.png");
-  const diamond = useLoader(THREE.TextureLoader, "/img/diamond.png");
-  const planeRef = useRef();
-
-  // Rotate the mesh in the Z direction
-  useFrame((state, delta) => {
-    if (planeRef.current) {
-      const rotationSpeed = (2 * Math.PI) / 6; // Defines the speed of rotation
-      planeRef.current.rotation.z += (rotationSpeed * delta) / 300; // Adjusts the rotation in the Z direction
-    }
-  });
 
   return (
     <>
@@ -70,52 +54,7 @@ export default function Game() {
       <Clouds position-z={-300} position-y={-5} scale={10} />
       <Model />
       <Player octree={octree} />
-      {/* Big one */}
-      <mesh
-        ref={planeRef}
-        position={[-400, 40, -700]}
-        rotation-y={Math.PI / 5}
-        rotation-z={Math.PI / 1.6}
-      >
-        <planeGeometry args={[1000, 1000]} />
-        <meshBasicMaterial transparent opacity={0.5} alphaMap={planet} />
-      </mesh>
-      {/* Middle one */}
-      <mesh
-        position={[-600, 500, -100]}
-        rotation-y={Math.PI / 0.41}
-        rotation-x={Math.PI / 2.5}
-        rotation-z={Math.PI / 2}
-      >
-        <planeGeometry args={[100, 100]} />
-        <meshBasicMaterial transparent opacity={0.8} alphaMap={planet3} />
-      </mesh>
-      {/* Small ones */}
-      <mesh position={[-600, 200, 600]} rotation-y={Math.PI / 1.5}>
-        <planeGeometry args={[60, 60]} />
-        <meshBasicMaterial transparent opacity={1.0} alphaMap={planet2} />
-      </mesh>
-      <mesh position={[-100, 200, 600]} rotation-y={Math.PI / 1}>
-        <planeGeometry args={[40, 40]} />
-        <meshBasicMaterial transparent opacity={1.0} alphaMap={planet2} />
-      </mesh>
-      <mesh
-        position={[400, 600, -700]}
-        rotation-y={-Math.PI / 6}
-        rotation-z={Math.PI / 1}
-      >
-        <planeGeometry args={[50, 50]} />
-        <meshBasicMaterial transparent opacity={0.8} alphaMap={planet2} />
-      </mesh>
-      <mesh
-        position={[400, 400, -100]}
-        rotation-y={-Math.PI / 3}
-        rotation-x={Math.PI / 2}
-        rotation-z={Math.PI / 0.8}
-      >
-        <planeGeometry args={[50, 50]} />
-        <meshBasicMaterial transparent opacity={0.8} alphaMap={planet2} />
-      </mesh>
+      <Planets />
     </>
   );
 }
---
+++ src/world-components/Planets.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useLoader } from "@react-three/fiber";
+import * as THREE from "three";
+
+export default function Planets() {
+  const planet = useLoader(THREE.TextureLoader, "/img/planet.png");
+  const planet2 = useLoader(THREE.TextureLoader, "/img/planet2.png");
+  const planet3 = useLoader(THREE.TextureLoader, "/img/planet3.png");
+
+  return (
+    <>
+      {/* Big one */}
+      <mesh
+        position={[-400, 40, -700]}
+        rotation-y={Math.PI / 5}
+        rotation-z={Math.PI / 1.6}
+      >
+        <planeGeometry args={[1000, 1000]} />
+        <meshBasicMaterial transparent opacity={0.5} alphaMap={planet} />
+      </mesh>
+      {/* Middle one */}
+      <mesh
+        position={[-600, 500, -100]}
+        rotation-y={Math.PI / 0.41}
+        rotation-x={Math.PI / 2.5}
+        rotation-z={Math.PI / 2}
+      >
+        <planeGeometry args={[100, 100]} />
+        <meshBasicMaterial transparent opacity={0.8} alphaMap={planet3} />
+      </mesh>
+      {/* Small ones */}
+      <mesh position={[-600, 200, 600]} rotation-y={Math.PI / 1.5}>
+        <planeGeometry args={[60, 60]} />
+        <meshBasicMaterial transparent opacity={1.0} alphaMap={planet2} />
+      </mesh>
+      <mesh position={[-100, 200, 600]} rotation-y={Math.PI / 1}>
+        <planeGeometry args={[40, 40]} />
+        <meshBasicMaterial transparent opacity={1.0} alphaMap={planet2} />
+      </mesh>
+      <mesh
+        position={[400, 600, -700]}
+        rotation-y={-Math.PI / 6}
+        rotation-z={Math.PI / 1}
+      >
+        <planeGeometry args={[50, 50]} />
+        <meshBasicMaterial transparent opacity={0.8} alphaMap={planet2} />
+      </mesh>
+      <mesh
+        position={[400, 400, -100]}
+        rotation-y={-Math.PI / 3}
+        rotation-x={Math.PI / 2}
+        rotation-z={Math.PI / 0.8}
+      >
+        <planeGeometry args={[50, 50]} />
+        <meshBasicMaterial transparent opacity={0.8} alphaMap={planet2} />
+      </mesh>
+    </>
+  );
+}
