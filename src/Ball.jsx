f24e0527f3028e5703f52188a349fbb7150b4994
Jerry Xia <jerrylxia@gmail.com>
2024-02-09 17:45:51 -0500

    Add matcap texture for ball

--- src/Ball.jsx
+++ src/Ball.jsx
@@ -1,8 +1,16 @@
+import { useMatcapTexture } from "@react-three/drei";
+
 export default function Ball({ radius }) {
+  const [matcapTexture] = useMatcapTexture("81ADB3_D6ECEE_BFDEE1_AFD1D7", 256);
+  // 65A0C7_C3E4F8_A7D5EF_97CAE9
+  // 80A6B4_D5E9EF_B2D0D9_C1DCE4
+  // 596773_B3C6CE_98AFB9_879AA8
+  // 487FC9_A8E7F8_88CCF2_70AFDE
+
   return (
     <mesh castShadow>
       <sphereGeometry args={[radius, 16, 16]} />
-      <meshStandardMaterial />
+      <meshMatcapMaterial matcap={matcapTexture} />
       {/* <meshNormalMaterial wireframe /> */}
     </mesh>
   );
